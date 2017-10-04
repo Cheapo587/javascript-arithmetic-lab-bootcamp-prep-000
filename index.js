514983179bb4216a56d1b7b42fabf1d1b04e2c18
Cheapo587 <ac.shultz587@gmail.com>
2017-10-04 20:26:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+//alex shultz, 10-4-2017, learn.co bootcamp prep (javascript integers/arithmetic)
+
+var number = 5
+
+function add(a, b){
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
+
+function inc(n){
+  return n++
+}
+
+function dec(n){
+  return n--
+}
+
+function makeInt(n){
+  return n.parseInt()
+}
